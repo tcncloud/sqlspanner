9a82dffcd68d70f29913f7aea8d7d4cbf9cfc795
Neal Cooper <neal.cooper@tcnp3.com>
2017-03-09 16:55:38 -0700

    added stub for driver.ValueConverter implementation

---
+++ valueConverter.go
@@ -0,0 +1,11 @@
+package sqlspanner
+
+import (
+	"database/sql/driver"
+)
+
+type valueConverter struct{}
+
+func (v valueConverter) ConvertValue(src interface{}) (driver.Value, error) {
+	return nil, nil
+}
