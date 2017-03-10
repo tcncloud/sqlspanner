98c129da33e7b56107c48b95dcac951eecff9b5e
Neal Cooper <neal.cooper@tcnp3.com>
2017-03-10 13:53:39 -0700

    fixed bad logic with rows.err field

--- rows.go
+++ rows.go
@@ -5,6 +5,7 @@ import (
 	"database/sql/driver"
 	"google.golang.org/api/iterator"
 	"io"
+	"fmt"
 )
 
 type rows struct {
@@ -23,14 +24,18 @@ func newRowsFromNextable(iter nextable) *rows {
 	r := &rows{
 		iter:   iter,
 		valuer: valueConverter{},
+		row: nil,
 	}
 
 	if r.iter == nil {
 		r.err = io.EOF
 		return r
 	}
+
+	fmt.Printf("new::: row: %+v  err:%+v  iter: %+v\n", r.row, r.err, r.iter)
 	r.iterate()
-	if r.err != nil {
+	fmt.Printf("after iter::: row: %+v  err:%+v  iter: %+v\n", r.row, r.err, r.iter)
+	if r.err == nil {
 		r.cols = r.row.ColumnNames()
 	}
 	return r
@@ -52,10 +57,11 @@ func (r *rows) iterate() {
 	if r.iter == nil {
 		r.err = io.EOF
 	}
-	if r.err != nil {
+	if r.err == nil {
 		//get the first row result now, so we can get the column names
 		// r.row only will exist if r.done is false
 		row, err := r.iter.Next()
+		fmt.Printf("iterate row: %+v  err: %+v\n", row, err)
 		if err == iterator.Done {
 			r.err = io.EOF
 		} else if err != nil {
@@ -64,6 +70,7 @@ func (r *rows) iterate() {
 			r.row = row
 		}
 	}
+	fmt.Printf("at end of iterate: row: %+v, err: %+v \n", r.row, r.err)
 }
 
 // because of how Next, and iterate work,  r.row  will always exist at this point
@@ -90,13 +97,17 @@ func (r *rows) handleRow(dest []driver.Value) {
 }
 
 func (r *rows) Next(dest []driver.Value) error {
+	fmt.Printf("row: %+v  err:%+v  iter: %+v\n", r.row, r.err, r.iter)
 	switch {
 	case r.row != nil: // first row gotten by an interator will be handled this way
+		fmt.Printf("took row not nil path\n")
 		r.handleRow(dest)
 		return r.err
 	case r.err != nil: // always return the last error we were given
+		fmt.Printf("took err path\n")
 		return r.err
 	default:
+		fmt.Printf("took default\n")
 		r.iterate()
 		if r.err != nil {
 			return r.err
