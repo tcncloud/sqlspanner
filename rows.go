12d241b52cb2f32e09b97a29c2204b817d998112
Neal Cooper <neal.cooper@tcnp3.com>
2017-03-10 10:19:56 -0700

    got rid of done field

--- rows.go
+++ rows.go
@@ -8,50 +8,58 @@ import (
 )
 
 type rows struct {
-	iter   *spanner.RowIterator
+	iter   nextable
 	row    *spanner.Row
 	valuer valueConverter
 	cols   []string
-	done   bool
 	err    error
 }
 
-// Some of our methods we support require having a spanner row already
-// this function sets this up so we can know the column names
 func newRowsFromSpannerIterator(iter *spanner.RowIterator) *rows {
+	return newRowsFromNextable(iter)
+}
+
+func newRowsFromNextable(iter nextable) *rows {
 	r := &rows{
 		iter:   iter,
-		done:   false,
 		valuer: valueConverter{},
 	}
 
 	if r.iter == nil {
-		r.done = true
+		r.err = io.EOF
 		return r
 	}
 	r.iterate()
-	if !r.done {
+	if r.err != nil {
 		r.cols = r.row.ColumnNames()
 	}
 	return r
 }
 
 func newRowsFromSpannerRow(row *spanner.Row) *rows {
-	//UNIMPLEMENTED
-	return nil
+	if row == nil {
+		return newRowsFromNextable(nil)
+	}
+	return &rows{
+		iter: nil,
+		valuer: valueConverter{},
+		row: row,
+		cols: row.ColumnNames(),
+	}
 }
 
 func (r *rows) iterate() {
-	if !r.done {
+	if r.iter == nil {
+		r.err = io.EOF
+	}
+	if r.err != nil {
 		//get the first row result now, so we can get the column names
 		// r.row only will exist if r.done is false
 		row, err := r.iter.Next()
 		if err == iterator.Done {
-			r.done = true
 			r.err = io.EOF
 		} else if err != nil {
 			r.err = err
-			r.done = true
 		} else {
 			r.row = row
 		}
@@ -60,35 +68,33 @@ func (r *rows) iterate() {
 
 // because of how Next, and iterate work,  r.row  will always exist at this point
 func (r *rows) handleRow(dest []driver.Value) {
+	row := r.row
+	r.row = nil
 	//set up generic pointers to pull of each row
-	pointers := make([]*spanner.GenericColumnValue, r.row.Size())
-	for i := 0; i < r.row.Size(); i++ {
+	pointers := make([]*spanner.GenericColumnValue, row.Size())
+	for i := 0; i < row.Size(); i++ {
 		pointers[i] = new(spanner.GenericColumnValue)
 	}
 	//read all the columns from the row
-	r.row.Columns(pointers)
+	row.Columns(pointers)
 	for i, col := range pointers {
 		driverVal, err := r.valuer.ConvertValue(col)
 		if err != nil {
 			// abort everything ever for this iterator
 			r.err = err
-			r.done = true
-			r.row = nil
 			return
 		}
 		//dest is the same size as columns, so we dont need to append
 		dest[i] = driverVal
 	}
-	// nil out the row, because it has been assigned to dest
-	r.row = nil
 }
 
 func (r *rows) Next(dest []driver.Value) error {
 	switch {
-	case r.done: // always return the last error we were given
-		return r.err
 	case r.row != nil: // first row gotten by an interator will be handled this way
 		r.handleRow(dest)
+		return r.err
+	case r.err != nil: // always return the last error we were given
 		return r.err
 	default:
 		r.iterate()
@@ -105,9 +111,15 @@ func (r *rows) Columns() []string {
 }
 
 func (r *rows) Close() error {
-	r.done = true
 	if r.iter != nil {
 		r.iter.Stop()
 	}
 	return nil
+}
+
+// So I don't have to be given a spanner.RowIterator,  I could be given anything
+// that returns a spanner.Row, and an error
+type nextable interface{
+	Next() (*spanner.Row, error)
+	Stop()
 }
