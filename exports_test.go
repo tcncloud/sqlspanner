b8ad9acd964a3fa760dab930e0d76ce638764017
Neal Cooper <neal.cooper@tcnp3.com>
2017-03-10 13:53:14 -0700

    updated fake spanner iterator

--- exports_test.go
+++ exports_test.go
@@ -1,7 +1,7 @@
 package sqlspanner
 
 import (
-	"io"
+	"google.golang.org/api/iterator"
 	"cloud.google.com/go/spanner"
 )
 // This file exports our private functions for testing
@@ -16,11 +16,11 @@ type TestNextable struct {
 	max int
 }
 func (n TestNextable) Next() (*spanner.Row, error) {
-	if n.cur > n.max {
-		return nil, io.EOF
+	if n.cur < n.max {
+		n.cur++
+		return spanner.NewRow([]string{"a", "b", "c"}, []interface{}{"1", "2", "3"})
 	}
-	n.cur++
-	return spanner.NewRow([]string{"a", "b", "c"}, []interface{}{"1", "2", "3"})
+	return nil, iterator.Done
 }
 
 func (n TestNextable) Stop() {
