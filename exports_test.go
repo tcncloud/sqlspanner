724939683d1080f43918ec13bb7ebd92e945bb20
Neal Cooper <neal.cooper@tcnp3.com>
2017-03-13 10:02:10 -0600

    minor changes

--- exports_test.go
+++ exports_test.go
@@ -15,20 +15,20 @@ type TestNextable struct {
 	cur int
 	max int
 }
-func (n TestNextable) Next() (*spanner.Row, error) {
+func (n *TestNextable) Next() (*spanner.Row, error) {
 	if n.cur < n.max {
-		n.cur++
+		n.cur += 1
 		return spanner.NewRow([]string{"a", "b", "c"}, []interface{}{"1", "2", "3"})
 	}
 	return nil, iterator.Done
 }
 
-func (n TestNextable) Stop() {
+func (n *TestNextable) Stop() {
 	n.cur = n.max
 }
 
-func NewTestNextable(iterations int) TestNextable {
-	return TestNextable{ cur: 0, max: iterations }
+func NewTestNextable(iterations int) *TestNextable {
+	return &TestNextable{ cur: 0, max: iterations }
 }
 
 
