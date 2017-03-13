02dfbd1b7763c90600b7a0cb71fc5490f81490e9
Neal Cooper <neal.cooper@tcnp3.com>
2017-03-13 12:51:17 -0600

    can parse simple typed arrays

--- valueConverter.go
+++ valueConverter.go
@@ -6,8 +6,8 @@ import (
 	"database/sql/driver"
 	"errors"
 	v1 "google.golang.org/genproto/googleapis/spanner/v1"
+	"cloud.google.com/go/civil"
 	"cloud.google.com/go/spanner"
-	"reflect"
 )
 
 type valueConverter struct{}
@@ -17,7 +17,6 @@ func (v valueConverter) ConvertValue(src interface{}) (driver.Value, error) {
 }
 
 func (v valueConverter) ConvertGenericCol(g *spanner.GenericColumnValue) (driver.Value, error) {
-	fmt.Printf("g %+v", g)
 	if g == nil {
 		return nil, nil
 	}
@@ -26,6 +25,7 @@ func (v valueConverter) ConvertGenericCol(g *spanner.GenericColumnValue) (driver
 	}
 	switch g.Type.Code {
 	case v1.TypeCode_TYPE_CODE_UNSPECIFIED:
+		return nil, fmt.Errorf("GenericColumnValue type code unspecified")
 	case v1.TypeCode_BOOL:
 		var val bool
 		g.Decode(&val)
@@ -43,7 +43,9 @@ func (v valueConverter) ConvertGenericCol(g *spanner.GenericColumnValue) (driver
 		g.Decode(&val)
 		return val, nil
 	case v1.TypeCode_DATE:
-		return nil, errors.New(UnimplementedError)
+		var val civil.Date
+		g.Decode(&val)
+		return val, nil
 	case v1.TypeCode_STRING:
 		var val string
 		g.Decode(&val)
@@ -52,15 +54,12 @@ func (v valueConverter) ConvertGenericCol(g *spanner.GenericColumnValue) (driver
 		var val []byte
 		g.Decode(&val)
 		return val, nil
-	case v1.TypeCode_ARRAY:
+	case v1.TypeCode_ARRAY:// [](basic type)  or []struct
 		if g.Type.ArrayElementType == nil {
 			return nil, fmt.Errorf("Recieved array TypeCode with nil ArrayElementType")
 		}
-		myArrType := setupArrayType(g.Type.Code, g.Type.ArrayElementType)
-		val := reflect.New(myArrType).Interface()
-		g.Decode(&val)
-		return val, nil
-	case v1.TypeCode_STRUCT:
+		return convertArrayType(g, g.Type.ArrayElementType)
+	case v1.TypeCode_STRUCT:// unsupported
 		return nil, errors.New(UnimplementedError)
 	default:
 	}
@@ -68,6 +67,45 @@ func (v valueConverter) ConvertGenericCol(g *spanner.GenericColumnValue) (driver
 }
 // recursively calls itself  till there is no more array type given
 // it is recursive, because it is possible to have an array of array of arrays etc.
-func setupArrayType(ty v1.TypeCode, arrType *v1.Type) reflect.Type {
-	return reflect.TypeOf(errors.New(UnimplementedError))
+func convertArrayType(g *spanner.GenericColumnValue, arrType *v1.Type) (driver.Value, error) {
+	if arrType == nil {
+		return nil, fmt.Errorf("recieved nil pointer when converting GenericColumnValue")
+	}
+	switch arrType.Code {
+	case v1.TypeCode_BOOL:
+		var val []spanner.NullBool
+		g.Decode(&val)
+		return val, nil
+	case v1.TypeCode_INT64:
+		var val []spanner.NullInt64
+		g.Decode(&val)
+		return val, nil
+	case v1.TypeCode_FLOAT64:
+		var val []spanner.NullFloat64
+		g.Decode(&val)
+		return val, nil
+	case v1.TypeCode_BYTES:
+		var val [][]byte
+		g.Decode(&val)
+		return val, nil
+	case v1.TypeCode_TIMESTAMP:
+		var val []spanner.NullTime
+		g.Decode(&val)
+		return val, nil
+	case v1.TypeCode_STRING:
+		var val []spanner.NullString
+		g.Decode(&val)
+		return val, nil
+	case v1.TypeCode_DATE:
+		var val []spanner.NullDate
+		g.Decode(&val)
+		return val, nil
+	case v1.TypeCode_STRUCT://spanner.NullRow?
+		_ = g.Value.GetListValue().GetValues()[0].GetStructValue().GetFields()
+		// create []reflect.StructField from map["string"]*ptypes.Value
+		return nil, errors.New(UnimplementedError)
+	default:
+		return nil, fmt.Errorf("not able to decoded type")
+	}
 }
+
