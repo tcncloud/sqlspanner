36171d5c94c09f90f84c59d95329fe12833e5e0b
Neal Cooper <neal.cooper@tcnp3.com>
2017-04-05 16:20:26 -0600

    better error handling

--- valueConverter.go
+++ valueConverter.go
@@ -43,10 +43,6 @@ import (
 
 type valueConverter struct{}
 
-func (v valueConverter) ConvertValue(src interface{}) (driver.Value, error) {
-	return nil, errors.New(UnimplementedError)
-}
-
 func (v valueConverter) ConvertGenericCol(g *spanner.GenericColumnValue) (driver.Value, error) {
 	if g == nil {
 		return nil, nil
@@ -58,33 +54,81 @@ func (v valueConverter) ConvertGenericCol(g *spanner.GenericColumnValue) (driver
 	case v1.TypeCode_TYPE_CODE_UNSPECIFIED:
 		return nil, fmt.Errorf("GenericColumnValue type code unspecified")
 	case v1.TypeCode_BOOL:
-		var val bool
-		g.Decode(&val)
-		return val, nil
+		var t bool
+		var nt spanner.NullBool
+		err := g.Decode(&t)
+		if err != nil {
+			err = g.Decode(&nt)
+			if err != nil {
+				return nil, err
+			}
+			return nt, nil
+		}
+		return t, nil
 	case v1.TypeCode_INT64:
-		var val int64
-		g.Decode(&val)
-		return val, nil
+		var t int64
+		var nt spanner.NullInt64
+		err := g.Decode(&t)
+		if err != nil {
+			err = g.Decode(&nt)
+			if err != nil {
+				return nil, err
+			}
+			return nt, nil
+		}
+		return t, nil
 	case v1.TypeCode_FLOAT64:
-		var val float64
-		g.Decode(&val)
-		return val, nil
+		var t float64
+		var nt spanner.NullFloat64
+		err := g.Decode(&t)
+		if err != nil {
+			err = g.Decode(&nt)
+			if err != nil {
+				return nil, err
+			}
+			return nt, nil
+		}
+		return t, nil
 	case v1.TypeCode_TIMESTAMP:
-		var val time.Time
-		g.Decode(&val)
-		return val, nil
+		var t time.Time
+		var nt spanner.NullTime
+		err := g.Decode(&t)
+		if err != nil {
+			err = g.Decode(&nt)
+			if err != nil {
+				return nil, err
+			}
+			return nt, nil
+		}
+		return t, nil
 	case v1.TypeCode_DATE:
-		var val civil.Date
-		g.Decode(&val)
-		return val, nil
+		var t civil.Date
+		var nt spanner.NullDate
+		err := g.Decode(&t)
+		if err != nil {
+			err = g.Decode(&nt)
+			if err != nil {
+				return nil, err
+			}
+			return nt, nil
+		}
+		return t, nil
 	case v1.TypeCode_STRING:
-		var val string
-		g.Decode(&val)
-		return val, nil
+		var t string
+		var nt spanner.NullString
+		err := g.Decode(&t)
+		if err != nil {
+			err = g.Decode(&nt)
+			if err != nil {
+				return nil, err
+			}
+			return nt, nil
+		}
+		return t, nil
 	case v1.TypeCode_BYTES:
 		var val []byte
-		g.Decode(&val)
-		return val, nil
+		err := g.Decode(&val)
+		return val, err
 	case v1.TypeCode_ARRAY: // [](basic type)  or []struct
 		if g.Type.ArrayElementType == nil {
 			return nil, fmt.Errorf("Recieved array TypeCode with nil ArrayElementType")
@@ -97,8 +141,6 @@ func (v valueConverter) ConvertGenericCol(g *spanner.GenericColumnValue) (driver
 	return nil, nil
 }
 
-// recursively calls itself  till there is no more array type given
-// it is recursive, because it is possible to have an array of array of arrays etc.
 func convertArrayType(g *spanner.GenericColumnValue, arrType *v1.Type) (driver.Value, error) {
 	if arrType == nil {
 		return nil, fmt.Errorf("recieved nil pointer when converting GenericColumnValue")
@@ -106,32 +148,32 @@ func convertArrayType(g *spanner.GenericColumnValue, arrType *v1.Type) (driver.V
 	switch arrType.Code {
 	case v1.TypeCode_BOOL:
 		var val []spanner.NullBool
-		g.Decode(&val)
-		return val, nil
+		err := g.Decode(&val)
+		return val, err
 	case v1.TypeCode_INT64:
 		var val []spanner.NullInt64
-		g.Decode(&val)
-		return val, nil
+		err := g.Decode(&val)
+		return val, err
 	case v1.TypeCode_FLOAT64:
 		var val []spanner.NullFloat64
-		g.Decode(&val)
-		return val, nil
+		err := g.Decode(&val)
+		return val, err
 	case v1.TypeCode_BYTES:
 		var val [][]byte
-		g.Decode(&val)
-		return val, nil
+		err := g.Decode(&val)
+		return val, err
 	case v1.TypeCode_TIMESTAMP:
 		var val []spanner.NullTime
-		g.Decode(&val)
-		return val, nil
+		err := g.Decode(&val)
+		return val, err
 	case v1.TypeCode_STRING:
 		var val []spanner.NullString
-		g.Decode(&val)
-		return val, nil
+		err := g.Decode(&val)
+		return val, err
 	case v1.TypeCode_DATE:
 		var val []spanner.NullDate
-		g.Decode(&val)
-		return val, nil
+		err := g.Decode(&val)
+		return val, err
 	case v1.TypeCode_STRUCT: //spanner.NullRow?
 		_ = g.Value.GetListValue().GetValues()[0].GetStructValue().GetFields()
 		// create []reflect.StructField from map["string"]*ptypes.Value
